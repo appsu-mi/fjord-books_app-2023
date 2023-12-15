c0bca2340006ae9b112fab611e7fa58889105b08
appsu-mi <natubate870@gmail.com>
2023-12-15 18:28:39 +0900

    first push.

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,7 +3,12 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'has editing authority' do
+    assert reports(:test_report).editable?(users(:owner))
+    assert_not reports(:test_report).editable?(users(:other))
+  end
+
+  test 'created_at to date_object' do
+    assert_equal reports(:test_report).created_at.to_date, reports(:test_report).created_on
+  end
 end
